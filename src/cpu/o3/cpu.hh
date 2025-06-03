861cd0b3f58da64384f8645e59ed2e552bb9678e
root <osman.yasar@tum.de>
2025-06-03 13:11:21 +0000

    cpu: add top-down methodology

--- src/cpu/o3/cpu.hh
+++ src/cpu/o3/cpu.hh
@@ -67,6 +67,7 @@
 #include "cpu/o3/rename.hh"
 #include "cpu/o3/rob.hh"
 #include "cpu/o3/scoreboard.hh"
+#include "cpu/o3/tdm.hh"
 #include "cpu/o3/thread_state.hh"
 #include "cpu/simple_thread.hh"
 #include "cpu/timebuf.hh"
@@ -612,6 +613,9 @@ class CPU : public BaseCPU
         /** Stat for total number of cycles the CPU spends descheduled due to a
          * quiesce operation or waiting for an interrupt. */
         statistics::Scalar quiesceCycles;
+        /** Top Down Methodology stats */
+        TopDownStats topDownStats;
+
     } cpuStats;
 
   public:
