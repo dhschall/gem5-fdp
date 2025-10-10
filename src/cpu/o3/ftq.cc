dhschall/gem5-upstream

cpu: Fix front-end prefetcher rework
The implementation puts all the prefetching into the front-end. Allows sending translations
and prefetches in parallel right in the front-end.
Much better integration with FDP and performance.
It works now with x86 in full system mode and was tested by running some serverless functions.

src/cpu/o3/ftq.cc
@@ -51,34 +51,75 @@ namespace o3
 
 
 /** Fetch Target Methods -------------------------------- */
-FetchTarget::FetchTarget(const PCStateBase &_start_pc, InstSeqNum _seqNum)
-    : ftSeqNum(_seqNum),
-      is_branch(false), taken(false),
-      bpu_history(nullptr)
+FetchTarget::FetchTarget(const FTQ& parent, const PCStateBase &_start_pc,
+                         InstSeqNum _seqNum)
+  : ftq(parent),
+    ftSeqNum(_seqNum),
+    is_branch(false), taken(false),
+    paddr(MaxAddr),
+    translation_done(false),
+    paddr_valid(false),
+    bpu_history(nullptr),
+    state(Initial)
 {
     set(startPC , _start_pc);
+    vaddr = startPC->instAddr() & ~(ftq.cacheBlkSize-1);
 }
 
+FetchTarget::~FetchTarget()
+{
+    assert(bpu_history == nullptr);
+    req = nullptr;
+    fault = nullptr;
+}
 
 void
-FetchTarget::finalize(const PCStateBase &exit_pc, InstSeqNum sn,
+FetchTarget::finalize(const PCStateBase &exit_pc, const Addr end_addr,
+                      InstSeqNum sn,
                       bool _is_branch, bool pred_taken,
                       const PCStateBase &pred_pc)
 {
+    assert((exit_pc.instAddr() & ~(ftq.cacheBlkSize-1)) == vaddr);
     set(endPC, exit_pc);
     set(predPC, pred_pc);
+    endAddr = end_addr;
     taken = pred_taken;
     is_branch = _is_branch;
 }
 
+
+Tick
+FetchTarget::finishTranslation(const Fault &_fault, const RequestPtr &_req,
+                           bool prefetch) {
+    if (paddr_valid) return 0;
+
+    fault = _fault;
+    DPRINTF(FTQ, "Finish translation %s, with %i, %i\n",
+                print(), _fault, fault);
+    if (_fault == NoFault) {
+        paddr = _req->getPaddr();
+        translation_done = true;
+        paddr_valid = true;
+    }
+
+    if (prefetch) {
+        state = (fault == NoFault) ? TranslationReady : TranslationFailed;
+    } else {
+        state = ReadyToFetch;
+    }
+    return curTick() - translation_start;
+}
 
 std::string
 FetchTarget::print()
 {
     std::stringstream ss;
     ss << "FT[" << ftSeqNum << "]: [0x" << std::hex
-        << startPC->instAddr() << "->0x" << endPC->instAddr()
+        << startPC->instAddr() << "->0x" << endAddr
         << "|B:" << is_branch
+        << "|T:" << taken
+        << "|S:" << state
+        << "|BA:0x" << vaddr
         << "]";
     return ss.str();
 }
@@ -88,6 +129,7 @@ FetchTarget::print()
 
 FTQ::FTQ(CPU *_cpu, const BaseO3CPUParams &params)
     : cpu(_cpu),
+      cacheBlkSize(cpu->cacheLineSize()),
       numThreads(params.numThreads),
       numEntries(params.numFTQEntries),
       stats(_cpu, this)
@@ -200,6 +242,25 @@ FTQ::forAllBackward(ThreadID tid, std::function<void(FetchTargetPtr&)> f)
     }
 }
 
+FetchTargetPtr
+FTQ::findNext(ThreadID tid, std::function<bool(FetchTargetPtr&)> f)
+{
+    for (auto it = ftq[tid].begin(); it != ftq[tid].end(); it++) {
+        if (f(*it)) return *it;
+    }
+    return nullptr;
+}
+
+FetchTargetPtr
+FTQ::findAfterHead(ThreadID tid, std::function<bool(FetchTargetPtr&)> f)
+{
+    auto it = ftq[tid].begin();
+    if (it == ftq[tid].end()) return nullptr;
+    for (it++; it != ftq[tid].end(); it++) {
+        if (f(*it)) return *it;
+    }
+    return nullptr;
+}
 
 
 void
@@ -239,7 +300,7 @@ FTQ::squashSanityCheck(ThreadID tid)
 bool
 FTQ::isHeadReady(ThreadID tid)
 {
-    return (ftqStatus[tid] != Invalid) && (ftq[tid].size() > 0);
+    return (ftqStatus[tid] != Invalid) && (ftq[tid].size() > 1);
 }
 
 
@@ -250,6 +311,15 @@ FTQ::readHead(ThreadID tid)
     if (ftq[tid].empty()) return nullptr;
 
     return ftq[tid].front();
+}
+
+FetchTargetPtr
+FTQ::readNextHead(ThreadID tid)
+{
+    if (ftqStatus[tid] == Invalid) return nullptr;
+    if (ftq[tid].size() < 2) return nullptr;
+
+    return *(++ftq[tid].begin());
 }
 
 
@@ -282,6 +352,17 @@ FTQ::updateHead(ThreadID tid)
     return ret_val;
 }
 
+bool
+FTQ::finishTranslation(ThreadID tid, const Fault &fault, RequestPtr &req)
+{
+    for (auto it = ftq[tid].begin(); it != ftq[tid].end(); it++) {
+        if ((*it)->req == req) {
+            (*it)->fault = fault;
+            return true;
+        }
+    }
+    return false;
+}
 
 
 void

dhschall/gem5-fdp

src/cpu/o3/ftq.cc
@@ -51,33 +51,78 @@ namespace o3
 {
 
 /** Fetch Target Methods -------------------------------- */
-FetchTarget::FetchTarget(const ThreadID _tid, const PCStateBase &_start_pc,
-                         InstSeqNum _seqNum)
-    : ftSeqNum(_seqNum),
-      tid(_tid),
-      is_branch(false),
-      taken(false),
-      bpuHistory(nullptr)
+FetchTarget::FetchTarget(const FTQ& parent, const ThreadID _tid,
+                         const PCStateBase &_start_pc, InstSeqNum _seqNum)
+  : ftq(parent),
+    ftSeqNum(_seqNum),
+    tid(_tid),
+    is_branch(false),
+    taken(false),
+    paddr(MaxAddr),
+    translation_done(false),
+    paddr_valid(false),
+    bpuHistory(nullptr),
+    state(Initial)
 {
-    set(startPC, _start_pc);
+    set(startPC , _start_pc);
+    vaddr = startPC->instAddr() & ~(ftq.cacheBlkSize-1);
+}
+
+FetchTarget::~FetchTarget()
+{
+    assert(bpuHistory == nullptr);
+    req = nullptr;
+    fault = nullptr;
 }
 
 void
-FetchTarget::finalize(const PCStateBase &exit_pc, bool _is_branch,
-                      bool pred_taken, const PCStateBase &pred_pc)
+FetchTarget::finalize(const PCStateBase &exit_pc, const Addr end_addr,
+                      InstSeqNum sn,
+                      bool _is_branch, bool pred_taken,
+                      const PCStateBase &pred_pc)
 {
+    assert((exit_pc.instAddr() & ~(ftq.cacheBlkSize-1)) == vaddr);
     set(endPC, exit_pc);
     set(predPC, pred_pc);
+    endAddr = end_addr;
     taken = pred_taken;
     is_branch = _is_branch;
+}
+
+
+Tick
+FetchTarget::finishTranslation(const Fault &_fault, const RequestPtr &_req,
+                           bool prefetch) {
+    if (paddr_valid) return 0;
+
+    fault = _fault;
+    DPRINTF(FTQ, "Finish translation %s, with %i, %i\n",
+                print(), _fault, fault);
+    if (_fault == NoFault) {
+        paddr = _req->getPaddr();
+        translation_done = true;
+        paddr_valid = true;
+    }
+
+    if (prefetch) {
+        state = (fault == NoFault) ? TranslationReady : TranslationFailed;
+    } else {
+        state = ReadyToFetch;
+    }
+    return curTick() - translation_start;
 }
 
 std::string
 FetchTarget::toString()
 {
     std::stringstream ss;
-    ss << "FT[" << ftSeqNum << "]: [0x" << std::hex << startPC->instAddr()
-       << "->0x" << endPC->instAddr() << "|B:" << is_branch << "]";
+    ss << "FT[" << ftSeqNum << "]: [0x" << std::hex
+        << startPC->instAddr() << "->0x" << endAddr
+        << "|B:" << is_branch
+        << "|T:" << taken
+        << "|S:" << state
+        << "|BA:0x" << vaddr
+        << "]";
     return ss.str();
 }
 
@@ -85,6 +130,8 @@ FetchTarget::toString()
 
 FTQ::FTQ(CPU *_cpu, const BaseO3CPUParams &params)
     : cpu(_cpu),
+      cacheBlkSize(cpu->cacheLineSize()),
+      numThreads(params.numThreads),
       numEntries(params.numFTQEntries),
       stats(_cpu, params.numFTQEntries)
 {
@@ -182,6 +229,28 @@ FTQ::forAllBackward(ThreadID tid, std::function<void(FetchTargetPtr &)> f)
     }
 }
 
+
+FetchTargetPtr
+FTQ::findNext(ThreadID tid, std::function<bool(FetchTargetPtr&)> f)
+{
+    for (auto it = ftq[tid].begin(); it != ftq[tid].end(); it++) {
+        if (f(*it)) return *it;
+    }
+    return nullptr;
+}
+
+FetchTargetPtr
+FTQ::findAfterHead(ThreadID tid, std::function<bool(FetchTargetPtr&)> f)
+{
+    auto it = ftq[tid].begin();
+    if (it == ftq[tid].end()) return nullptr;
+    for (it++; it != ftq[tid].end(); it++) {
+        if (f(*it)) return *it;
+    }
+    return nullptr;
+}
+
+
 void
 FTQ::insert(ThreadID tid, FetchTargetPtr fetchTarget)
 {
@@ -218,7 +287,7 @@ FTQ::squashSanityCheck(ThreadID tid)
 bool
 FTQ::isHeadReady(ThreadID tid)
 {
-    return (ftqStatus[tid] != Invalid) && (ftq[tid].size() > 0);
+    return (ftqStatus[tid] != Invalid) && (ftq[tid].size() > 1);
 }
 
 FetchTargetPtr
@@ -233,6 +302,17 @@ FTQ::readHead(ThreadID tid)
 
     return ftq[tid].front();
 }
+
+
+FetchTargetPtr
+FTQ::readNextHead(ThreadID tid)
+{
+    if (ftqStatus[tid] == Invalid) return nullptr;
+    if (ftq[tid].size() < 2) return nullptr;
+
+    return *(++ftq[tid].begin());
+}
+
 
 bool
 FTQ::popHead(ThreadID tid)
@@ -263,6 +343,20 @@ FTQ::popHead(ThreadID tid)
     stats.removals++;
     return ret_val;
 }
+
+
+bool
+FTQ::finishTranslation(ThreadID tid, const Fault &fault, RequestPtr &req)
+{
+    for (auto it = ftq[tid].begin(); it != ftq[tid].end(); it++) {
+        if ((*it)->req == req) {
+            (*it)->fault = fault;
+            return true;
+        }
+    }
+    return false;
+}
+
 
 void
 FTQ::printFTQ(ThreadID tid)